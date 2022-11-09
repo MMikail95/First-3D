b99db1b741a60092a2d57838520ba2246a58724b
MMikail95 <mmikailyazar@gmail.com>
2022-11-09 16:22:30 +0300

    Oyun Bitti

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -8,7 +8,7 @@ public class PlayerLife : MonoBehaviour
     bool dead = false;
     private void Update()
     {
-        if (transform.position.y < -1f && !dead)
+        if (transform.position.y < -5f && !dead)
         {
             Die();
         }
